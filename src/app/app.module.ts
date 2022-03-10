f0321338e2566e04ebe2f88961db7f5b0f2d85f0
EL MAHDI BENZINA <benzina.elmahdi@gmail.com>
2022-03-10 14:45:48 +0100

    FEATURE/PO-I5 : footer component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { SidebarComponent } from './components/dashboard/sidebar/sidebar.component';
 import { ContentComponent } from './components/dashboard/content/content.component';
+import { FooterComponent } from './components/dashboard/content/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DashboardComponent,
     SidebarComponent,
-    ContentComponent
+    ContentComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
